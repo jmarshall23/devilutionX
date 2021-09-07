5a4b4baeb36fff491205f4ccee1d33f95344c610
Justin Marshall <justinmarshall20@gmail.com>
2021-09-07 07:15:44 -0700

    Added dundump tool. Removed duntext files.

---
+++ DunDump/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+
+namespace DunDump
+{
+    class Program
+    {
+        static void SaveDun(string filename)
+        {
+            string dunFile = filename;
+            string outFile = Path.GetDirectoryName(dunFile) + "/" + Path.GetFileNameWithoutExtension(dunFile) + ".duntext";
+
+            Console.WriteLine("Opening dungeon file {0}", dunFile);
+
+            using (StreamWriter writer = File.CreateText(outFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(dunFile, FileMode.Open)))
+                {
+                    int width = reader.ReadInt16();
+                    int height = reader.ReadInt16();
+
+                    writer.WriteLine("x,y,tilid");
+                    //writer.WriteLine("Width," + width + ",0");
+                    //writer.WriteLine("Height," + height + ",0");
+
+                    Console.WriteLine("Width {0} Height {1}", width, height);
+
+                    ushort[] tileLayer = new ushort[reader.BaseStream.Length - 4];
+                    for (int i = 0; i < (reader.BaseStream.Length - 4) / 2; i++)
+                        tileLayer[i] = reader.ReadUInt16();
+
+                    for (int j = 0; j < height; j++)
+                    {
+                        for (int i = 0; i < width; i++)
+                        {
+                            int tileId = (tileLayer[j * width + i]) - 1;
+
+                            writer.WriteLine(i + "," + j + "," + tileId);
+                        }
+                    }
+
+                    if (reader.BaseStream.Position != reader.BaseStream.Length)
+                        throw new Exception("invalid dun parse");
+                }
+            }
+        }
+
+        static void SaveTil(string filename)
+        {
+            string tilFile = filename;
+            string outFile = Path.GetDirectoryName(tilFile) + "/" + Path.GetFileNameWithoutExtension(tilFile) + ".tiltext";
+
+            Console.WriteLine("Opening til file {0}", tilFile);
+
+            using (StreamWriter writer = File.CreateText(outFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(tilFile, FileMode.Open)))
+                {
+                    writer.WriteLine("index,top,right,left,bottom");
+
+                    long numEntries = reader.BaseStream.Length / (2 * 4);
+
+                    for(int i = 0; i < numEntries; i++)
+                    {
+                        ushort top = reader.ReadUInt16();
+                        ushort right = reader.ReadUInt16();
+                        ushort left = reader.ReadUInt16();
+                        ushort bottom = reader.ReadUInt16();
+
+                        writer.WriteLine(i + "," + top + "," + right + "," + left + "," + bottom);
+                    }
+
+                    if (reader.BaseStream.Position != reader.BaseStream.Length)
+                        throw new Exception("invalid til parse");
+                }
+            }
+        }
+
+        static void SaveMin(string filename)
+        {
+            string tilFile = filename;
+            string outFile = Path.GetDirectoryName(tilFile) + "/" + Path.GetFileNameWithoutExtension(tilFile) + ".mintext";
+
+            Console.WriteLine("Opening min file {0}", tilFile);
+
+            using (StreamWriter writer = File.CreateText(outFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(tilFile, FileMode.Open)))
+                {
+                    writer.WriteLine("index,blockval");
+
+                    long numEntries = reader.BaseStream.Length / 2;
+
+                    for (int i = 0; i < numEntries; i++)
+                    {
+                        ushort val = reader.ReadUInt16();                        
+
+                        writer.WriteLine(i + "," + val);
+                    }
+
+                    if (reader.BaseStream.Position != reader.BaseStream.Length)
+                        throw new Exception("invalid min parse");
+                }
+            }
+        }
+
+        static void SaveSol(string filename)
+        {
+            string tilFile = filename;
+            string outFile = Path.GetDirectoryName(tilFile) + "/" + Path.GetFileNameWithoutExtension(tilFile) + ".soltext";
+
+            Console.WriteLine("Opening sol file {0}", tilFile);
+
+            using (StreamWriter writer = File.CreateText(outFile))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(tilFile, FileMode.Open)))
+                {
+                    writer.WriteLine("index,val");
+
+                    long numEntries = reader.BaseStream.Length;
+
+                    for (int i = 0; i < numEntries; i++)
+                    {
+                        byte val = reader.ReadByte();
+
+                        writer.WriteLine(i + "," + val);
+                    }
+
+                    if (reader.BaseStream.Position != reader.BaseStream.Length)
+                        throw new Exception("invalid sol parse");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Dun files
+            {
+                string[] files = System.IO.Directory.GetFiles(args[0], "*.dun");
+
+                foreach (string f in files)
+                    SaveDun(f);
+            }
+
+            // Til files
+            {
+                string[] files = System.IO.Directory.GetFiles(args[0], "*.til");
+
+                foreach (string f in files)
+                    SaveTil(f);
+            }
+
+            // min files
+            {
+                string[] files = System.IO.Directory.GetFiles(args[0], "*.min");
+
+                foreach (string f in files)
+                    SaveMin(f);
+            }
+
+            // sol files
+            {
+                string[] files = System.IO.Directory.GetFiles(args[0], "*.sol");
+
+                foreach (string f in files)
+                    SaveSol(f);
+            }
+
+        }
+    }
+}
